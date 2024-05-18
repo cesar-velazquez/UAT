f55ed8f375346138afe27abcdcfdb081e3b3e826
Fernando Jose Santiago Tulley <ferjotulley@gmail.com>
2024-05-18 17:05:50 -0600

    Update font size

--- scripts/change-fonts.js
+++ scripts/change-fonts.js
@@ -1,24 +1,34 @@
 
 const btnAumentarFuente = document.getElementById('font_size');
-const parrafo = document.querySelector('.parrafo');
-const titulos = document.querySelectorAll("h2");
+//const parrafo = document.querySelector('.parrafo');
+const subtitulos = document.querySelectorAll("h2");
 const parrafos = document.querySelectorAll("p");
 
-let tamFuenteParrafos = 1.3; // Tamaño de fuente inicial
+let tamFuenteParrafos = 1.3; // Tamaño de fuente parrrafos
+let tamFuenteSubtitulos = 1.5; // Tamaño de fuente subtitulos
 
 btnAumentarFuente.addEventListener('click', function() {
 
-    console.log("Se hizo click");
-
-    tamFuenteParrafos += 0.25; // Aumentar 0.2 rem en cada click
+    tamFuenteParrafos += 0.25; // Aumentar 0.25 rem en cada click
+    tamFuenteSubtitulos +=0.25;
 
     if (tamFuenteParrafos > 1.8) {
         tamFuenteParrafos = 1.3; // Restablecer a 1.3 rem si se supera 1.8 rem
+    }
+
+    if (tamFuenteSubtitulos > 2) {
+        tamFuenteSubtitulos = 1.5; // Restablecer a 1.5 rem si se supera 2 rem
     }
 
     parrafos.forEach(parrafo => {
         parrafo.style.fontSize = tamFuenteParrafos + 'rem';
         parrafo.style.transitionDuration = "1s";
     });
+
+    subtitulos.forEach(subtitulo => {
+        subtitulo.style.fontSize = tamFuenteSubtitulos + 'rem';
+        subtitulo.style.transitionDuration = "1s";
+    });
     
 });
+
