1dfa9a4c1b323068c57d8ad6d45c530f5e223236
Fernando Jose Santiago Tulley <ferjotulley@gmail.com>
2024-05-20 11:39:54 -0600

    Update link resaltado

--- scripts/links.js
+++ scripts/links.js
@@ -1,2 +1,22 @@
 
-const parrafos = document.querySelectorAll("a");
+
+const botonResaltar = document.getElementById("link_resaltar");
+const enlaces = document.querySelectorAll("a");
+
+let resaltar = false;
+
+botonResaltar.addEventListener("click", function() {
+    
+    console.log("Se hizo click");
+
+    resaltar = !resaltar;
+
+    enlaces.forEach(enlace => {
+        if (resaltar) {
+        enlace.classList.add("resaltado");
+        } else {
+        enlace.classList.remove("resaltado");
+        }
+    });
+
+});
